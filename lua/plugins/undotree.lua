6c56b696faac8ffb004a64f3cc03a60129d10239
YJ Eum <yjeum@cmu.edu>
2024-06-24 14:02:47 -0400

    undotree dir

--- lua/plugins/undotree.lua
+++ lua/plugins/undotree.lua
@@ -1,6 +1,6 @@
 vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
 
-local path = os.getenv("HOME") .. "/.local/share/nvim/undodir"
+local path = os.getenv("HOME") .. "/.local/state/nvim/undo"
 
 vim.opt.undodir = path
 vim.opt.undofile = true
