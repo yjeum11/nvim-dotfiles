74a147e8cb56c33849d8fa229fe726aa71398906
Youngjin Eum <yeum@ece024.ece.local.cmu.edu>
2025-02-04 12:11:34 -0500

    oil keymaps mimic fzf-lua

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,7 +1,11 @@
 require("oil").setup({
     view_options = {
         show_hidden = true
-    }
+    },
+    keymaps = {
+        ["<C-v>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-x>"] = { "actions.select", opts = { horizontal = true } },
+    },
 })
 
 vim.keymap.set("n", "<leader>o", ":Oil --float<CR>")
