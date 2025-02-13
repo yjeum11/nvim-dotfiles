1db61bd1b5e5578ef57650c07eb063a3901901f4
Youngjin Eum <yeum@ece024.ece.local.cmu.edu>
2025-02-13 13:39:13 -0500

    fzf keybindings

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,2 +1,3 @@
-vim.keymap.set("n", "<leader>f", ":Files<CR>")
-
+vim.keymap.set("n", "<leader>ff", ":Files<CR>")
+vim.keymap.set("n", "<leader>fg", ":GitFiles<CR>")
+vim.keymap.set("n", "<leader>fb", ":Buffers<CR>")
