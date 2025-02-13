043454f600fff05b4e1bf4c40da731030f7a0b19
YJ Eum <yjeum11@gmail.com>
2025-02-13 13:47:35 -0500

    fzf git grep

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,3 +1,7 @@
+vim.api.nvim_create_user_command("GGrep", "call fzf#vim#grep('git grep --line-number -- '.fzf#shellescape(<q-args>), fzf#vim#with_preview({'dir': systemlist('git rev-parse --show-toplevel')[0]}), <bang>0)", {})
+
 vim.keymap.set("n", "<leader>ff", ":Files<CR>")
 vim.keymap.set("n", "<leader>fg", ":GitFiles<CR>")
 vim.keymap.set("n", "<leader>fb", ":Buffers<CR>")
+vim.keymap.set("n", "<leader>fG", ":GGrep<CR>")
+
